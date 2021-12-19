7fa52bbb309c89cbfef54acff921fa35d1801e8c
VeyronSakai <yuukisakai123@gmail.com>
2021-12-19 17:52:27 +0900

    abc175 a

--- abc175/src/bin/a.rs
+++ abc175/src/bin/a.rs
@@ -1,9 +1,26 @@
+use nalgebra::max;
 use proconio::input;
 
 fn main() {
     input! {
-        a: i32,
-        b: i32,
+        s: String,
     }
-    println!("{}", a - b);
+
+    let mut ans = 0;
+    let mut count = 0;
+
+    let mut pre_char = '_';
+
+    for char in s.chars() {
+        if pre_char == char {
+            count += 1;
+            ans = max(ans, count);
+        } else {
+            pre_char = char;
+            count = 1;
+            ans = max(ans, count);
+        }
+    }
+
+    println!("{}", ans);
 }
