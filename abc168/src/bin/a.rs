51dafe54af204709df661dbd45ba0e95019efd48
VeyronSakai <yuukisakai123@gmail.com>
2021-12-19 17:12:40 +0900

    abc168 a

---
+++ abc168/src/bin/a.rs
@@ -0,0 +1,16 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: i32,
+    }
+
+    let ans = match n % 10 {
+        2 | 4 | 5 | 7 | 9 => "hon",
+        0 | 1 | 6 | 8 => "pon",
+        3 => "bon",
+        _ => unreachable!(),
+    };
+
+    println!("{}", ans);
+}
