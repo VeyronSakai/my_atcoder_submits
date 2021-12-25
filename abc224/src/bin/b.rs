f3d01383c058a9e7057e5cb7fed7b861f929e420
VeyronSakai <yuukisakai123@gmail.com>
2021-12-26 01:54:40 +0900

    abc224

--- abc224/src/bin/b.rs
+++ abc224/src/bin/b.rs
@@ -1,9 +1,26 @@
 use proconio::input;
+use proconio::fastout;
 
+#[fastout]
 fn main() {
     input! {
-        a: i32,
-        b: i32,
+        h: usize,
+        w: usize,
+        a: [[i64; w] ; h]
     }
-    println!("{}", a - b);
+
+    for i2 in 0..h {
+        for i1 in 0..i2 {
+            for j2 in 0..w {
+                for j1 in 0..j2 {
+                    if a[i1][j1] + a[i2][j2] > a[i2][j1] + a[i1][j2] {
+                        println!("No");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    println!("Yes");
 }
