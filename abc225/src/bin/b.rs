3c834d887c283bb95cc7d59084c42b3144d20fb0
VeyronSakai <yuukisakai123@gmail.com>
2021-12-26 06:15:04 +0900

    abc225 b

--- abc225/src/bin/b.rs
+++ abc225/src/bin/b.rs
@@ -4,8 +4,27 @@ use proconio::fastout;
 #[fastout]
 fn main() {
     input! {
-        a: i32,
-        b: i32,
+        n: usize,
+        input: [(i32, i32); n - 1]
     }
-    println!("{}", a - b);
+
+    let mut info = vec![0; n];
+
+    for &val in &input {
+        info[(val.0 - 1) as usize] += 1;
+        info[(val.1 - 1) as usize] += 1;
+    }
+
+    for i in 0..n - 1 {
+        if info[i] == n - 1 {
+            continue;
+        }
+
+        if info[i] != 1 {
+            println!("No");
+            return;
+        }
+    }
+
+    println!("Yes");
 }
