fcfb8ff00b7a167df949391c0cbc5cfe3b422b9f
VeyronSakai <yuukisakai123@gmail.com>
2021-12-26 23:55:16 +0900

    abc221

--- abc221/src/bin/c.rs
+++ abc221/src/bin/c.rs
@@ -1,9 +1,61 @@
+use nalgebra::max;
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-        a: i32,
-        b: i32,
+        n: Chars,
     }
-    println!("{}", a - b);
+
+    let mut ans = 0;
+
+    let k = n.len();
+
+    for bit in 0..1 << k {
+        // 分離する
+        let mut a: Vec<u64> = Vec::new();
+        let mut b: Vec<u64> = Vec::new();
+
+        for i in 0..k {
+            if bit >> i & 1 == 1 {
+                a.push(n[i].to_digit(10).unwrap() as u64);
+            } else {
+                b.push(n[i].to_digit(10).unwrap() as u64);
+            }
+        }
+
+        // どちらかが空の場合は何もしない
+        if a.is_empty() || b.is_empty() {
+            continue;
+        }
+
+        // 昇順にソート
+        a.sort();
+        b.sort();
+
+        // 0が一番大きい数字の場合は何もしない
+        if *a.last().unwrap() == 0 || *b.last().unwrap() == 0 {
+            continue;
+        }
+
+        let mut a_num = 0;
+        let mut b_num = 0;
+
+        let mut ten = 1;
+
+        for i in 0..a.len() {
+            a_num += a[i] * ten;
+            ten *= 10;
+        }
+
+        ten = 1;
+        for i in 0..b.len() {
+            b_num += b[i] * ten;
+            ten *= 10;
+        }
+
+        ans = max(ans, a_num * b_num);
+    }
+
+    println!("{}", ans);
 }
