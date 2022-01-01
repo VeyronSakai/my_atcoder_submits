8ad057ba9f191d5802cdf7cabe08c7e4733930db
Yuki Sakai <yuukisakai123@gmail.com>
2022-01-01 14:38:49 +0900

    memo

--- abc223/src/bin/c.rs
+++ abc223/src/bin/c.rs
@@ -2,8 +2,32 @@ use proconio::input;
 
 fn main() {
     input! {
-        a: i32,
-        b: i32,
+        n: usize,
+        vec: [(i32, i32); n]
     }
-    println!("{}", a - b);
+
+    let a = vec.iter().map(|x| x.0).collect::<Vec<_>>();
+    let b = vec.iter().map(|x| x.1).collect::<Vec<_>>();
+
+    // 導火線の長さが最大でO(10^8) cm くらいになりそうなので、1秒ずつシミュレーションするとO(10^8)くらいの計算量になりそうでダメ
+
+    // 累積和(cumulative sum)で行く
+    // 左からi番目の導火線が燃え尽きる時間を記録する
+
+    let mut left = vec![0_f32; n];
+    let mut right = vec![0_f32; n];
+
+    left[0] = a[0] as f32 / b[0] as f32;
+    right[n - 1] = a[n - 1] as f32 / b[n - 1] as f32;
+
+    for i in 1..n {
+        left[i] = left[i - 1] + a[i] as f32 / b[i] as f32;
+        right[n - i - 1] = right[n - i] + a[n - i - 1] as f32 / b[n - i - 1] as f32;
+    }
+
+    // ~~~|--|--====
+    // ~~~--|--|====
+
+    // の2パターンのみ
+    
 }
--- abc226/src/bin/c.rs
+++ abc226/src/bin/c.rs
@@ -1,11 +1,51 @@
+use std::cmp::min;
+use im_rc::HashSet;
 use proconio::input;
 use proconio::fastout;
 
 #[fastout]
 fn main() {
     input! {
-        a: i32,
-        b: i32,
+        n: usize,
     }
-    println!("{}", a - b);
+
+    let mut t: Vec<i64> = Vec::new();
+    let mut k: Vec<i64> = Vec::new();
+    let mut a: Vec<Vec<usize>> = Vec::new();
+
+    for _ in 0..n {
+        input! {
+            input_t: i64,
+            input_k:i64,
+            input_a:[usize; input_k],
+        }
+
+        t.push(input_t);
+        k.push(input_k);
+        a.push(input_a);
+    }
+
+    let mut memo_time = vec![0; n];
+    let mut memo_skill: Vec<HashSet<i64>> = vec![HashSet::new(); n];
+
+    memo_time[0] = t[0];
+    memo_skill[0].insert(0); // 0番目の技は習得済み
+
+    // 技を習得するのにかかる最小時間を配列にメモしていく
+    // i番目技を習得したときにすでに習得している他の技もメモしていく
+    for i in 1..n {
+        let mut min_time = i64::MAX;
+
+        if a[i].is_empty() {
+            min_time = 0;
+        }
+
+        for val in &a[i] {
+            min_time = min(min_time, memo_time[*val - 1]);
+        }
+
+        memo_time[i] = min_time + t[i];
+    }
+
+    for val in &a[n - 1] {}
 }
