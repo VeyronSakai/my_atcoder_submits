47a93d7394a3421eab6e9caf43c9ff18b380358f
Yuki Sakai <yuukisakai123@gmail.com>
2022-01-24 02:18:20 +0900

    abc 236 b

--- abc236/src/bin/b.rs
+++ abc236/src/bin/b.rs
@@ -1,11 +1,27 @@
+use std::collections::BTreeSet;
+use im_rc::HashMap;
 use proconio::input;
 use proconio::fastout;
 
 #[fastout]
 fn main() {
     input! {
-        a: i32,
-        b: i32,
+        n: usize,
+        a: [i32; 4 * n - 1],
     }
-    println!("{}", a - b);
+
+    let mut map : HashMap<i32, i32> = HashMap::new();
+
+    for v in &a {
+        let mut count = *map.get(v).unwrap_or(&0);
+        count += 1;
+        map.insert(*v, count);
+    }
+
+    for v in 1..=n {
+        let key = v as i32;
+        if *map.get(&key).unwrap() == 3{
+            println!("{}", v);
+        }
+    }
 }
