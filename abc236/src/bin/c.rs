00c4f7a3ee87940f2fc6134d74ee8dd305a05c7d
Yuki Sakai <yuukisakai123@gmail.com>
2022-01-25 00:20:56 +0900

    abc 236 c

--- abc236/src/bin/c.rs
+++ abc236/src/bin/c.rs
@@ -1,11 +1,29 @@
+use std::collections::HashMap;
 use proconio::input;
 use proconio::fastout;
 
 #[fastout]
 fn main() {
     input! {
-        a: i32,
-        b: i32,
+        n: usize,
+        m: usize,
+        s: [String; n],
+        t: [String; m],
     }
-    println!("{}", a - b);
+
+    let mut map: HashMap<String, bool> = HashMap::new();
+
+    for stop in &t {
+        map.insert(stop.clone(), true);
+    }
+
+    for stop in &s {
+        let ans = map.get(stop).unwrap_or(&false);
+
+        if *ans {
+            println!("Yes");
+        }else{
+            println!("No");
+        }
+    }
 }
