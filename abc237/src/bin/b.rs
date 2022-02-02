fb7a0cac7e53f478da9a43368835e37f58323934
Yuki Sakai <yuukisakai123@gmail.com>
2022-02-03 01:16:54 +0900

    abc237 b

--- abc237/src/bin/b.rs
+++ abc237/src/bin/b.rs
@@ -1,11 +1,28 @@
-use proconio::input;
 use proconio::fastout;
+use proconio::input;
 
 #[fastout]
 fn main() {
     input! {
-        a: i32,
-        b: i32,
+        h: usize,
+        w: usize,
     }
-    println!("{}", a - b);
+
+    let mut a: Vec<Vec<i64>> = Vec::new();
+
+    for _ in 0..h {
+        input! {
+            tmp_a: [i64; w]
+        }
+
+        a.push(tmp_a);
+    }
+
+    for i in 0..w {
+        for j in 0..h {
+            print!("{} ", a[j][i]);
+        }
+
+        println!("");
+    }
 }
