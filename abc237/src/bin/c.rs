ad515d063dcb8a807c564f44dd94daa5f37c2d7c
Yuki Sakai <yuukisakai123@gmail.com>
2022-02-04 02:14:24 +0900

    abc237

--- abc237/src/bin/c.rs
+++ abc237/src/bin/c.rs
@@ -4,35 +4,25 @@ use proconio::input;
 #[fastout]
 fn main() {
     input! {
-        s: String,
+        s: String
     }
 
-    let s: Vec<char> = s.chars().collect();
+    let s1 = s.trim_matches(|c| c == 'a').to_string();
+    let t1 = s1.chars().rev().collect::<String>();
 
-    let mut r = s.len() as i32 - 1;
+    if s1 == t1 {
+        let s2 = s.chars().take_while(|&c| c == 'a').count();
+        let t2 = s.chars().rev().take_while(|&c| c == 'a').count();
 
-    let length = s.len() as i32;
-
-    // 右から見ていって最後にaが出てくる場所を調べる。最初の文字がa出なければ s.len() がrに入る
-    for i in (0..length).rev() {
-        if s[i as usize] == 'a' {
-            continue;
-        }
-
-        r = i;
-
-        break;
-    }
-
-    let str = &s[0..r as usize + 1];
-    println!("{:?}", str);
-
-    for i in 0..str.len() {
-        if str[i] != str[str.len() - 1 - i] {
+        if s2 <= t2 {
+            println!("Yes");
+            return;
+        } else {
             println!("No");
             return;
         }
+    } else {
+        println!("No");
+        return;
     }
-
-    println!("Yes");
 }
