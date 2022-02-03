cf858ccb17447bc235c679e4e2e06e17b61cbc98
Yuki Sakai <yuukisakai123@gmail.com>
2022-02-04 01:31:58 +0900

    fix

--- abc237/src/bin/c.rs
+++ abc237/src/bin/c.rs
@@ -9,38 +9,22 @@ fn main() {
 
     let s: Vec<char> = s.chars().collect();
 
-    let mut l = 0;
     let mut r = s.len() as i32 - 1;
 
-    let length = s.len() as i32 - 1;
-
-    // 左から見ていって最後にaが出てくる場所を調べる。最初の文字がaでなければ-1がlに入る
-    for i in 0..length - 1 {
-        if s[i as usize] == 'a' {
-            continue;
-        }
-
-        l = i;
-        break;
-    }
+    let length = s.len() as i32;
 
     // 右から見ていって最後にaが出てくる場所を調べる。最初の文字がa出なければ s.len() がrに入る
-    for i in length..0 {
+    for i in (0..length).rev() {
         if s[i as usize] == 'a' {
             continue;
         }
 
         r = i;
+
         break;
     }
 
-    // 文字列を切り出す
-    let str = if *s.last().unwrap() != 'a' {
-        &s[l as usize..r as usize + 1]
-    } else {
-        &s[l as usize..r as usize]
-    };
-
+    let str = &s[0..r as usize + 1];
     println!("{:?}", str);
 
     for i in 0..str.len() {
