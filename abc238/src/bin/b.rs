258307dfbaa9de17c011243eed6829e5552b3bec
Yuki Sakai <yuukisakai123@gmail.com>
2022-02-08 00:05:29 +0900

    abc238 b

--- abc238/src/bin/b.rs
+++ abc238/src/bin/b.rs
@@ -1,11 +1,37 @@
+use std::cmp::max;
 use proconio::input;
 use proconio::fastout;
 
 #[fastout]
 fn main() {
     input! {
-        a: i32,
-        b: i32,
+        n: usize,
+        a: [usize; n],
     }
-    println!("{}", a - b);
+
+    let mut array = [false; 361];
+
+    array[0] = true;
+    array[360] = true;
+
+    let mut cur: usize = 0;
+
+    for i in 0..a.len() {
+        cur += a[i];
+        cur = cur % 360;
+        array[cur] = true;
+    }
+
+    let mut count = 0;
+    let mut ans = 0;
+
+    for i in 0..361 {
+        count += 1;
+        if array[i] {
+            ans = max(ans, count);
+            count = 0;
+        }
+    }
+
+    println!("{}", ans);
 }
